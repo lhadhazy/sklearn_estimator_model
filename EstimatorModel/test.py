2a43fceb81c4de71cb8199a61669ea26a9170d49
Hadházy László <lhadhazy@hotmail.com>
2019-02-13 16:20:10 +0100

    first commit

---
+++ EstimatorModel/test.py
@@ -0,0 +1,17 @@
+'''
+Created on Feb 13, 2019
+
+@author: lhadhazy
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
